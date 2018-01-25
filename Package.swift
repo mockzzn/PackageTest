c9b73fbcf7a6166baaa5241a249f20ebf14f524b
ZZN <dream_at@sina.com>
2018-01-25 17:32:42 +0800

    test

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "PackageTest",
+    name: "MyApp",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "PackageTest",
-            targets: ["PackageTest"]),
+            name: "MyApp",
+            targets: ["MyApp"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,10 +21,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "PackageTest",
-            dependencies: ["PackageTest"]),
-        .testTarget(
-            name: "MyAppTests",
-            dependencies: ["MyApp"]),
+            name: "MyApp",
+            dependencies: []),
+        // .testTarget(
+        //     name: "MyAppTests",
+        //     dependencies: ["MyApp"]),
     ]
 )
